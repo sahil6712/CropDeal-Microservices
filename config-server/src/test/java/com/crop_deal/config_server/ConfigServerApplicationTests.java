a82fdb5f050c91ba79150611a521d86edeeeab1c
Sahil <sahilkhemariya327@gmail.com>
2025-03-20 11:46:32 +0530

    [Sahil] Add. Config server added for central configuration management

---
+++ config-server/src/test/java/com/crop_deal/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crop_deal.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
